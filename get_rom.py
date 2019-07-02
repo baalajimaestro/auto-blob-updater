f084ad5c0e64de58d7992eea110ed319a3a7f4a6
baalajimaestro <baalajimaestro@computer4u.com>
2019-07-02 14:03:35 +0000

    Violet

--- get_rom.py
+++ get_rom.py
@@ -1,12 +1,12 @@
 from requests import get
 from datetime import datetime as dt
 import json
-with open('whyred_stable.json', 'wb') as load:
-    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/stable/whyred.json").content)
-fw_stable = json.loads(open('whyred_stable.json').read())
-with open('whyred_weekly.json', 'wb') as load:
-    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/weekly/whyred.json").content)
-fw_weekly = json.loads(open('whyred_weekly.json').read())
+with open('violet_stable.json', 'wb') as load:
+    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/stable/violet.json").content)
+fw_stable = json.loads(open('violet_stable.json').read())
+with open('violet_weekly.json', 'wb') as load:
+    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/weekly/violet.json").content)
+fw_weekly = json.loads(open('violet_weekly.json').read())
 stable_date = dt.strptime(fw_stable[1]["date"], "%Y-%m-%d")
 weekly_date = dt.strptime(fw_weekly[1]["date"], "%Y-%m-%d")
 if stable_date > weekly_date:
