40a7ee717cc54589c42d3a111a37242aadfe946c
Dyneteve <sound0020@gmail.com>
2019-07-17 15:54:06 +0300

    Hi violet

--- get_rom.py
+++ get_rom.py
@@ -1,26 +1,11 @@
 from requests import get
 from datetime import datetime as dt
 import json
-with open('whyred_stable.json', 'wb') as load:
-    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/stable/whyred.json").content)
-fw_stable = json.loads(open('whyred_stable.json').read())
-with open('whyred_weekly.json', 'wb') as load:
-    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/weekly/whyred.json").content)
+with open('violet_weekly.json', 'wb') as load:
+    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/weekly/violet.json").content)
 fw_weekly = json.loads(open('whyred_weekly.json').read())
-stable_date = dt.strptime(fw_stable[1]["date"], "%Y-%m-%d")
 weekly_date = dt.strptime(fw_weekly[1]["date"], "%Y-%m-%d")
-if stable_date > weekly_date:
-    URL="https://bigota.d.miui.com/"
-    version=fw_stable[1]["versions"]["miui"]
-    with open('/tmp/version','wb') as load:
-        load.write(str.encode(version))
-    URL+=version
-    URL+="/"
-    file=fw_stable[1]["file"]
-    file=file[10:]
-    URL+=file
-    print("Fetching Stable ROM......")
-else:
+if weekly_date:
     URL="https://bigota.d.miui.com/"
     version=fw_weekly[1]["versions"]["miui"]
     with open('/tmp/version','wb') as load:
--- runner_user.sh
+++ runner_user.sh
@@ -11,116 +11,100 @@
 
 
 ##### Build Env Dependencies
-build_env()
-{
-. "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/telegram
-TELEGRAM_TOKEN=$(cat /tmp/tg_token)
-export TELEGRAM_TOKEN
-tg_sendinfo "<code>[MaestroCI]: Vendor Cron Job rolled!</code>"
-cd ~
-git clone https://github.com/akhilnarang/scripts > /dev/null 2>&1
-cd scripts
-bash setup/android_build_env.sh  > /dev/null 2>&1
-echo "Build Dependencies Installed....."
-sudo unlink /usr/bin/python
-sudo apt-get install p7zip-full p7zip-rar -y > /dev/null 2>&1
-sudo ln -s /usr/bin/python2.7 /usr/bin/python
-sudo apt install brotli -y  > /dev/null 2>&1
-cd ..
-rm -rf scripts
+build_env() {
+    . "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/telegram
+    TELEGRAM_TOKEN=$(cat /tmp/tg_token)
+    export TELEGRAM_TOKEN
+    tg_sendinfo "<code>Vendor Cron Job rolled!</code>"
+    cd ~
+    git clone https://github.com/akhilnarang/scripts > /dev/null 2>&1
+    cd scripts
+    bash setup/android_build_env.sh  > /dev/null 2>&1
+    echo "Build Dependencies Installed....."
+    sudo unlink /usr/bin/python
+    sudo apt-get install p7zip-full p7zip-rar -y > /dev/null 2>&1
+    sudo ln -s /usr/bin/python2.7 /usr/bin/python
+    sudo apt install brotli -y  > /dev/null 2>&1
+    cd ..
+    rm -rf scripts
 }
 ##### Build Configs
 
-build_conf()
-{
-mkdir repo
-cd repo
-git config --global user.email "baalajimaestro@computer4u.com"
-git config --global user.name "baalajimaestro"
-curl -sL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-sudo python3 get-pip.py > /dev/null 2>&1
-sudo python3 -m pip install requests  > /dev/null 2>&1
-export LOC=$(cat /tmp/loc)
+build_conf() {
+    mkdir repo
+    cd repo
+    git config --global user.email "sound0020@gmail.com"
+    git config --global user.name "Dyneteve"
+    curl -sL https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    sudo python3 get-pip.py > /dev/null 2>&1
+    sudo python3 -m pip install requests  > /dev/null 2>&1
+    export LOC=$(cat /tmp/loc)
 }
 
 ##### Initialise Repo with lineage-16.0
 
-init_repo()
-{
-echo "Repo initialised......."
-repo init -u https://github.com/MoKee/android.git -b mkp --depth=1 > /dev/null 2>&1
-echo "Repo Syncing started......"
-repo sync -j20 --no-tags --no-clone-bundle -c > /dev/null 2>&1
-echo -e "\e[32mRepo Synced....."
+init_repo() {
+    echo "Repo initialised......."
+    repo init -u hhttps://github.com/PixelExperience/manifest -b pie --depth=1 > /dev/null 2>&1
+    echo "Repo Syncing started......"
+    repo sync -j$(nproc) --no-tags --no-clone-bundle -c > /dev/null 2>&1
+    echo -e "\e[32mRepo Synced....."
 }
 
 ##### Clone DT and gear up for blob extraction
 
-# git clone https://github.com/baalajimaestro/LineageOS_DT device/xiaomi/whyred > /dev/null 2>&1
-dt()
-{
-git clone https://github.com/GuaiYiHu/android_device_xiaomi_whyred device/xiaomi/whyred > /dev/null 2>&1
-git clone https://github.com/GuaiYiHu/android_vendor_xiaomi_whyred vendor/xiaomi/whyred > /dev/null 2>&1
-cd device/xiaomi/whyred
+dt() {
+    git clone https://github.com/PixelExperience-Devices/device_xiaomi_violet -b pie device/xiaomi/violet > /dev/null 2>&1
+    git clone https://github.com/PixelExperience-Devices/vendor_xiaomi_violet -b pie vendor/xiaomi/violet > /dev/null 2>&1
+    cd device/xiaomi/violet
 }
 ##### Fetch MIUI-Chinese ROM latest
-rom()
-{
-mkdir extract
-sudo mv $LOC/get_rom.py get_rom.py
-python3 get_rom.py
-unzip rom.zip -d miui > /dev/null 2>&1
-cd miui
+rom() {
+    mkdir extract
+    sudo mv $LOC/get_rom.py get_rom.py
+    python3 get_rom.py
+    unzip rom.zip -d miui > /dev/null 2>&1
+    cd miui
 }
 ##### Workaround for > Android 8.1 using brotli
-dec_brotli()
-{
-brotli --decompress system.new.dat.br
-brotli --decompress vendor.new.dat.br
-echo "Brotli decompressed."
+dec_brotli() {
+    brotli --decompress system.new.dat.br
+    brotli --decompress vendor.new.dat.br
+    echo "Brotli decompressed."
 }
 ##### Convert our dat files into raw images
-sdatimg()
-{
-curl -sLo sdat2img.py https://raw.githubusercontent.com/xpirt/sdat2img/master/sdat2img.py
-python3 sdat2img.py system.transfer.list system.new.dat > /dev/null 2>&1
-python3 sdat2img.py vendor.transfer.list vendor.new.dat vendor.img > /dev/null 2>&1
-mv system.img ../extract
-mv vendor.img ../extract
+sdatimg() {
+    curl -sLo sdat2img.py https://raw.githubusercontent.com/xpirt/sdat2img/master/sdat2img.py
+    python3 sdat2img.py system.transfer.list system.new.dat > /dev/null 2>&1
+    python3 sdat2img.py vendor.transfer.list vendor.new.dat vendor.img > /dev/null 2>&1
+    mv system.img ../extract
+    mv vendor.img ../extract
 }
 ##### Workaround the inability to mount images on drone CI
-extract()
-{
-cd ../extract
-mkdir system
-7z x system.img -y -osystem > /dev/null 2>&1
-mkdir vendor
-7z x vendor.img -y -ovendor > /dev/null 2>&1
-cd ..
+extract() {
+    cd ../extract
+    mkdir system
+    7z x system.img -y -osystem > /dev/null 2>&1
+    mkdir vendor
+    7z x vendor.img -y -ovendor > /dev/null 2>&1
+    cd ..
 }
+
 ##### Here's the blecc megik
-gen_blob()
-{
-bash extract-files.sh extract > /dev/null 2>&1
-bash setup-makefiles.sh > /dev/null 2>&1
-echo "Blobs Generated!"
+gen_blob() {
+    bash extract-files.sh extract > /dev/null 2>&1
+    bash setup-makefiles.sh > /dev/null 2>&1
+    echo "Blobs Generated!"
 }
 ##### Push the vendor
-push_vendor()
-{
-cd /home/ci/repo/vendor/xiaomi/whyred
-git init
-git add .
-git checkout -b $(cat /tmp/version)
-git commit -m "[MaestroCI]: Re-gen Blobs" --signoff
-git remote rm origin
-git remote add origin https://baalajimaestro:$(cat /tmp/GH_TOKEN)@github.com/baalajimaestro/vendor_xiaomi_whyred.git
-git push --force origin $(cat /tmp/version)
-tg_sendinfo "<code>Checked out and pushed Vendor Blobs for MIUI Version $(cat /tmp/version)</code>"
-echo "Job Successful!"
+push_vendor() {
+    cd /home/ci/repo/vendor/xiaomi/violet
+    git add .
+    git commit -m "violet: Re-gen blobs from MIUI $(cat /tmp/version)" --signoff
+    git push origin pie
+    tg_sendinfo "<code>Checked out and pushed Vendor Blobs for MIUI Version $(cat /tmp/version)</code>"
+    echo "Job Successful!"
 }
-
-
 
 build_env
 build_conf
