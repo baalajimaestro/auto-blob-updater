25fff567ef13ef148b25f1e5cbb64f0879d52d36
Baalaji <baalajimaestro@computer4u.com>
2019-07-17 18:51:55 +0530

    Update get_rom.py

--- get_rom.py
+++ get_rom.py
@@ -3,7 +3,7 @@ from datetime import datetime as dt
 import json
 with open('violet_weekly.json', 'wb') as load:
     load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/weekly/violet.json").content)
-fw_weekly = json.loads(open('whyred_weekly.json').read())
+fw_weekly = json.loads(open('violet_weekly.json').read())
 weekly_date = dt.strptime(fw_weekly[1]["date"], "%Y-%m-%d")
 if weekly_date:
     URL="https://bigota.d.miui.com/"
