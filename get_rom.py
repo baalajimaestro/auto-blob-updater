01addd5d07fb1399306603838e5b75932afa9fe6
baalajimaestro <baalajimaestro@computer4u.com>
2019-08-08 10:14:46 +0530

    Rom-Fetcher: Inline with XiaomiUpdater JSON Changes
    We prefer China ROMs over global coz they release earlier
    
    Signed-off-by: baalajimaestro <baalajimaestro@computer4u.com>

--- get_rom.py
+++ get_rom.py
@@ -1,14 +1,11 @@
 from requests import get
 from datetime import datetime as dt
 import json
-with open('whyred_stable.json', 'wb') as load:
-    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/stable/whyred.json").content)
-fw_stable = json.loads(open('whyred_stable.json').read())
-with open('whyred_weekly.json', 'wb') as load:
-    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/weekly/whyred.json").content)
-fw_weekly = json.loads(open('whyred_weekly.json').read())
-stable_date = dt.strptime(fw_stable[1]["date"], "%Y-%m-%d")
-weekly_date = dt.strptime(fw_weekly[1]["date"], "%Y-%m-%d")
+with open('whyred.json', 'wb') as load:
+    load.write(get("https://raw.githubusercontent.com/XiaomiFirmwareUpdater/xiaomifirmwareupdater.github.io/master/data/devices/latest/whyred.json").content)
+fw = json.loads(open('whyred.json').read())
+stable_date = dt.strptime(fw[1]["date"], "%Y-%m-%d")
+weekly_date = dt.strptime(fw[3]["date"], "%Y-%m-%d")
 if stable_date > weekly_date:
     URL="https://bigota.d.miui.com/"
     version=fw_stable[1]["versions"]["miui"]
@@ -16,18 +13,18 @@ if stable_date > weekly_date:
         load.write(str.encode(version))
     URL+=version
     URL+="/"
-    file=fw_stable[1]["file"]
+    file=fw[1]["filename"]
     file=file[10:]
     URL+=file
     print("Fetching Stable ROM......")
 else:
     URL="https://bigota.d.miui.com/"
-    version=fw_weekly[1]["versions"]["miui"]
+    version=fw[3]["versions"]["miui"]
     with open('/tmp/version','wb') as load:
         load.write(str.encode(version))
     URL+=version
     URL+="/"
-    file=fw_weekly[1]["file"]
+    file=fw[3]["filename"]
     file=file[10:]
     URL+=file
     print("Fetching Weekly ROM......")
